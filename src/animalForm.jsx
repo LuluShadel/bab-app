4c3232bb5d1b7484c4f532d914c58ac75e0686eb
Lucile THIEULIN <lucile.thieulin96@gmail.com>
2025-06-08 22:32:29 +0200

    ajout de list dans form

--- src/animalForm.jsx
+++ src/animalForm.jsx
@@ -11,6 +11,8 @@ const AnimalForm = ({ onClose, onSubmit }) => {
     okChien: false,
     okChat: false,
     okChild: false,
+    adoption:false,
+    rechercheFa:false,
   });
 
   const handleChange = (e) => {
@@ -32,9 +34,12 @@ const AnimalForm = ({ onClose, onSubmit }) => {
     race: formData.race,
     ddn: formData.ddn || null, 
     dpt: formData.dpt, 
+    lieu: formData.lieu,
     okChien: formData.okChien,
     okChat: formData.okChat,
-    okChild: formData.okChild
+    okChild: formData.okChild,
+    adoption: formData.adoption,
+    rechercheFa: formData.rechercheFa,
   };
 
   onSubmit(newAnimal);  // Envoie un objet propre à la fonction handleAddAnimal
@@ -50,12 +55,32 @@ const AnimalForm = ({ onClose, onSubmit }) => {
           <input name="type" placeholder="Type" onChange={handleChange} className="border p-2 rounded" />
           <input name="race" placeholder="Race" onChange={handleChange} className="border p-2 rounded" />
           <input name="ddn" placeholder="Date de naissance" type="date" onChange={handleChange} className="border p-2 rounded" />
+          <input name="lieu" placeholder="FA ou Pension etc..." onChange={handleChange} className="border p-2 rounded" />
           <input name="dpt" placeholder="Département" onChange={handleChange} className="border p-2 rounded" />
           <div className="flex gap-4">
             <label><input type="checkbox" name="okChien" onChange={handleChange} /> OK chien</label>
             <label><input type="checkbox" name="okChat" onChange={handleChange} /> OK chat</label>
             <label><input type="checkbox" name="okChild" onChange={handleChange} /> OK enfant</label>
           </div>
+          <div className="mb-4">
+  <label className="mr-4">
+    <input
+      type="checkbox"
+      checked={formData.adoption}
+      onChange={(e) => setFormData({ ...formData, adoption: e.target.checked })}
+    />
+    À l'adoption
+  </label>
+
+  <label className="ml-4">
+    <input
+      type="checkbox"
+      checked={formData.rechercheFa}
+      onChange={(e) => setFormData({ ...formData, rechercheFa: e.target.checked })}
+    />
+    Recherche FA
+  </label>
+</div>
           <div className="flex justify-end gap-3 mt-2">
             <button type="button" onClick={onClose} className="px-4 py-2 bg-gray-300 rounded">Annuler</button>
             <button type="submit" className="px-4 py-2 bg-green-500 text-white rounded">Ajouter</button>
